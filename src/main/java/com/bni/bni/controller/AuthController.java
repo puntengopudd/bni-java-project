a7db7dc9810b0403a153458dc627a3d05bd3ed47
puntengopudd <akmalshidqi17@gmail.com>
2025-06-05 14:34:56 +0700

    Tambahan Pesan Baru Lagi

--- src/main/java/com/bni/bni/controller/AuthController.java
+++ src/main/java/com/bni/bni/controller/AuthController.java
@@ -44,6 +44,7 @@ public class AuthController {
             response.put("status", 200);
             response.put("token", token);
             response.put("message", "Login successful");
+            response.put("message", "Ciee Berhasil Login");
             return ResponseEntity.ok(response);
         } else {
             response.put("status", 401);
